aa59a8e2774fef8aab816379c40d141376d98035
darioskal <dipasquale.d@tiscali.it>
2017-06-11 15:21:50 +0200

    error repaired

--- wsil/management/commands/github_crawler.py
+++ wsil/management/commands/github_crawler.py
@@ -2,12 +2,27 @@ import gzip
 from urllib import request
 import json
 import threading
+import logging
+logger = logging.getLogger(__name__)
+
 from wsil.models import RepositoryUsingIt
+
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand
 
 allowed_types = ['CreateEvent']
 count_dict = dict()
-file = 'wsil/script/last-date.txt'
+file = 'wsil/management/commands/last-date.txt'
+
+
+class Command(BaseCommand):
+    help = "Populate db from GitHub repositories"
+
+    def _create_language_stats(self):
+        start()
+
+    def handle(self, *args, **options):
+        self._create_language_stats()
 
 
 def start():
@@ -18,10 +33,10 @@ def start():
         line = f.readline()
         first_y, first_m, first_d, first_h = line.split("-")
 
-    for y in range(int(first_y), 2016):
-        for m in range(int(first_m), 12):
-            for d in range(int(first_d), 31):
-                for h in range(int(first_h), 23):
+    for y in range(int(first_y), 2017):
+        for m in range(int(first_m), 13):
+            for d in range(int(first_d), 32):
+                for h in range(int(first_h), 24):
                     yy = str(y)
                     mm = str(m) if m >= 10 else "0" + str(m)
                     dd = str(d) if d >= 10 else "0" + str(d)
@@ -34,6 +49,9 @@ def start():
                         print(ex)
                     last_y, last_m, last_d, last_h = yy, mm, dd, hh
                     analyze_file("file.gz")
+                    first_m = 1
+                    first_d = 1
+                    first_h = 0
                 save_into_db()
                 word = last_y + "-" + last_m + "-" + last_d + "-" + last_h
                 t = threading.Thread(target=save_into_file, args=(word, ))
@@ -69,3 +87,5 @@ def save_into_db():
         except ObjectDoesNotExist as odne:
             language_instance = RepositoryUsingIt.create(language_name=language, language_count=count_dict[language])
             language_instance.save()
+    logger.info("Data saved into db")
+
