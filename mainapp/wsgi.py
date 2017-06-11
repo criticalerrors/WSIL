a9f9fa969cb9a9f8c3bcd3c57a3aff74d7d9a047
darioskal <dipasquale.d@tiscali.it>
2017-06-11 12:46:44 +0200

    config updated

--- mainapp/wsgi.py
+++ mainapp/wsgi.py
@@ -13,7 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from django.conf import settings
 from whitenoise.django import DjangoWhiteNoise
 
-settings.configure()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mainapp.settings")
 
 application = get_wsgi_application()
