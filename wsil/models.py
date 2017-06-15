d25ed2754dedf7af20d2f22be447ca7e40db942f
fabionapoli34 <FABIONAPOLI34@GMAIL.COM>
2017-06-15 12:37:13 +0200

    Index.html e HOME..

--- wsil/models.py
+++ wsil/models.py
@@ -51,25 +51,62 @@ class Features(models.Model):
 
 
 
-"""QuestionOnIt(?tag, ?count)"""
-
 """InterestOverTimeLanguage(?language_name, ?date, ?interest_rate)"""
 
+class InterestOverTimeLanguage(models.Model):
+    language_name = models.CharField(unique= True, max_length=30)
+    date = models.CharField( null=True)
+    interest_rate = models.CharField(max_length=10, null=True)
+
+
+
 """InterestOverTimeFrameworkLibrary(?fw_or_lib, ?date, ?interest_rate)"""
+class InterestOverTimeFrameworkLibrary(models.Model):
+    fw_or_lib = models.CharField(unique=True, max_length=30)
+    date = models.CharField(null=True)
+    interest_rate = models.CharField(max_length=10, null=True)
 
 """ InterestByRegionLanguage(?language, ?region, ?interest_rate)"""
+class InterestByRegionLanguage(models.Model):
+    language = models.CharField(unique=True, max_length=30)
+    region = models.CharField(max_length=30, null=True)
+    interest_rate = models.CharField(max_length=10, null=True)
 
 """InterestByRegionFrameworkLibrary(?fw_or_lib, ?region, ?interest_rate)"""
-
+class InterestByRegionFrameworkLibrary(models.Model):
+    fw_or_lib = models.CharField(unique=True, max_length=30)
+    date = models.CharField(null=True)
+    interest_rate = models.CharField(max_length=10, null=True)
 """Course(?course_id, ?slug, ?course_type, ?course_name, ?logo, ?photo_url,
 ?description, ?workload, ?url)"""
+class Course(models.Model):
+    course_id = models.CharField(unique=True, max_length=30)
+    slug =  models.CharField(max_length=30, null=True)
+    course_type =  models.CharField(max_length=30, null=True)
+    logo = models.URLField(null=True)
+    photo_url = models.URLField(null=True)
+    description = models.CharField(max_length=150, null=True)
+    workload = models.CharField(max_length=100, null=True)
+    url = models.URLField(null=True)
+
+
 
 """CoursePartner(?course_id, ?partner_id, ?partner_name, ?partner_image)"""
 
+class CoursePartner(models.Model):
+    partner_id = models.CharField(max_length=30, unique= True)
+    course_id = models.ManyToManyField(Course, null=True)
+    partner_name = models.CharField(max_length=30, null=True)
+    partner_image =  models.URLField(null=True)
 """Job(?job_title, ?description, ?post_date, ?company_name, ?company_url,
 ?location_name, ?lat, ?lon, ?query)"""
 
-
-class Suggestion(models.Model):
-    keyword = models.CharField(max_length=30)
-    suggested_keyword = models.CharField(max_length=30)
+class Job(models.Model):
+    job_title = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=150, null=True)
+    post_date = models.CharField(max_length=10, null=True)
+    company_name = models.CharField(max_length=30, null=True)
+    company_url = models.URLField(null=True)
+    location_name =  models.CharField(max_length=30, null=True)
+    lat =  models.CharField(max_length=30, null=True)
+    query =  models.CharField(max_length=30, null=True)
