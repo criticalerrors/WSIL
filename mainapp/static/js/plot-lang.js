27f8179766e4c8a981cb898fa44b74b3d610b08e
darioskal <dipasquale.d@tiscali.it>
2017-06-17 10:03:19 +0200

    aggiunti url

--- mainapp/static/js/plot-lang.js
+++ mainapp/static/js/plot-lang.js
@@ -2,32 +2,32 @@
  * Created by ddipa on 15/06/2017.
  */
 
-function getPlot(lang, id, type) {
- fetch('http://127.0.0.1:8000/api/interest_time/'+ lang.toLowerCase() +'?format=json')
-    .then(function(data) {
-        return data.json();
-    })
-    .then(function(json) {
-        if (json.length == 0) {
-            document.getElementById(id).innerHTML = "<h1>No data available</h1>";
-            return;
-        }
-        var x = [];
-        var y = [];
-        for(var i = 0; i < json.length; i++) {
-            x[i] = json[i]['date'];
-            y[i] = json[i]['interest_rate'];
-        }
-        var data = [
-            {
-                x: x,
-                y: y,
-                type: type
+function getPlot(lang, id, type, url) {
+    fetch(url + '?format=json')
+        .then(function(data) {
+            return data.json();
+        })
+        .then(function(json) {
+            if (json.length == 0) {
+                document.getElementById(id).innerHTML = "<h1>No data available</h1>";
+                return;
             }
-        ];
-        Plotly.newPlot(id, data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+            var x = [];
+            var y = [];
+            for(var i = 0; i < json.length; i++) {
+                x[i] = json[i]['date'];
+                y[i] = json[i]['interest_rate'];
+            }
+            var data = [
+                {
+                    x: x,
+                    y: y,
+                    type: type
+                }
+            ];
+            Plotly.newPlot(id, data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
