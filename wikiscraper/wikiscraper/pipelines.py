6f1be8b4c484ab22189167e9adf2634f5a9cb0a3
mattiatomeo <tomeo.mat@gmail.com>
2017-06-17 16:16:43 +0200

    Fixed license, url formatting and fk Now the scraper library_or_framework manage correctly the many to many relation with the table language. If the scraper does not find the language on the table, He will add it. All the activity of data cleaning is managed by the scraper now. The WikiscraperPipeline now update only the database

--- wikiscraper/wikiscraper/pipelines.py
+++ wikiscraper/wikiscraper/pipelines.py
@@ -5,20 +5,29 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import re
+# We need to access directly to the Django's model in order to manipulate
+# the many to many relations
+from wsil.models import LibraryOrFramework, Language
+from django.core.exceptions import ObjectDoesNotExist
 
 class WikiscraperPipeline(object):
     def process_item(self, item, spider):
-        item['description'][0] = self.cleanhtml(item['description'][0])
-        for key in item.keys(): # All elements are lists of only one element.
-            item[key] = item[key][0] # We extract the element to remove the list
+        fw_lib_obj = item['item']
+        for key in fw_lib_obj.keys(): # All elements are lists with only one element.
+            fw_lib_obj[key] = fw_lib_obj[key][0] # We extract the single element to remove the list
 
-        item.save()
+        row = LibraryOrFramework(**fw_lib_obj) # All the fields addend by the scraper are extracted
+        row.save()
+
+        languages = item['language']
+        for lang in languages:
+            lang_to_add = None
+            try:
+                lang_to_add = Language.objects.get(name=lang)
+            except ObjectDoesNotExist as ex: # The language does not exist in the db
+                lang_to_add = Language(name=lang) # So we add it ourselves
+                lang_to_add.save()
+            finally:
+                row.language.add(lang_to_add)
+
         return item;
-
-
-    @staticmethod
-    def cleanhtml(raw_html):
-        cleanr = re.compile('<.*?>')
-        cleantext = re.sub(cleanr, '', raw_html)
-        return cleantext
--- wikiscraper/wikiscraper/spiders/lof.py
+++ wikiscraper/wikiscraper/spiders/lof.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 from wikiscraper.items import FeaturesItem, LibraryOrFrameworkItem
 from scrapy.http import Request
@@ -12,9 +13,6 @@ class LibraryOrFrameworkSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        last_request = None
-        #language = elem.xpath('//*[@id="mw-content-text"]/div/h3[position()<15]/span/text()').extract()
-
         for elem in response\
                         .xpath('//*[@id="mw-content-text"]/div/table[position()<18 and position()>2]/tr[position()>1]'):
             # Framework or library name extraction
@@ -29,17 +27,26 @@ class LibraryOrFrameworkSpider(scrapy.Spider):
             cells = list(elem.xpath('./td')) # In some table there is also a column named 'Language'
             # We start from the last column and we go backward
             lic = cells[-1].xpath('./text() | ./a/text()').extract_first()
-            stable_version = cells[-2].xpath('./text()').extract_first()
-            release_date = cells[-3].xpath('./text()').extract_first()
+            release_date = cells[-2].xpath('./text()').extract_first()
+            stable_version = cells[-3].xpath('./text()').extract_first()
 
             # Getting the url of the detailed page
-            fw_lib_url = 'https://en.wikipedia.org' + elem.xpath('./th/a/@href').extract_first()
+            base_path = elem.xpath('./th/a/@href').extract_first()
+            if 'http' in base_path: # Some entries have a link to their homepage. We will exclude it
+                continue
+            fw_lib_url = 'https://en.wikipedia.org' + base_path
             self.log('Sending request to ' + fw_lib_url)
             request = Request(fw_lib_url, callback=self.get_details)
+
 
             request.meta['name'] = name
             if language[0] != "Others":
                 request.meta['language'] = language
+            else:
+                # On the table named Others, there are a column with the frameworks' languages
+                lang = cells[-4].xpath('.').extract_first()
+                request.meta['language'] = [l.lstrip() for l in cleanhtml(lang).split(',')]
+
             request.meta['stable_version'] = stable_version
             request.meta['rel_date'] = release_date
             request.meta['license'] = lic
@@ -47,18 +54,23 @@ class LibraryOrFrameworkSpider(scrapy.Spider):
             yield request
 
 
+    # Continue the parsing in the framework/library main page
     def get_details(self, response):
         self.log('Starting the second parsing phase')
         loader = ItemLoader(item=LibraryOrFrameworkItem(), response=response)
 
+        # Load the values obtained in the first phase
         loader.add_value('name', response.meta['name'])
 
-        if 'language' is response.meta:
-            loader.add_value('language', response.meta['language'])
+        language = response.meta['language']
+
         loader.add_value('stable_release', response.meta['stable_version'])
-        # Waiting for the update of FrameworkOrLibrary model
         loader.add_value('release_date', response.meta['rel_date'])
-        loader.add_xpath('description', '//*[@id="mw-content-text"]/div/p[1] | //*[@id="mw-content-text"]/p[1]')
+
+
+        descr = response.xpath('//*[@id="mw-content-text"]/div/p[1] | //*[@id="mw-content-text"]/p[1]').extract_first()
+        cleaned_descr = cleanhtml(descr)
+        loader.add_value('description', cleaned_descr)
 
         license_found = False
         for row in response\
@@ -66,17 +78,22 @@ class LibraryOrFrameworkSpider(scrapy.Spider):
             header = row.xpath('./th/a/text() | ./th/text()').extract_first()
             key, value = self.get_key_value(header, row)
             if key:
-                if key == 'license':
+                if key == 'license': # If we find the license in the main page, we will use it
                     license_found = True
                 loader.add_value(key, value)
-        # If it not found the license in the main page
-        # it will use the license found in the start page
+        # If we not found the license in the main page
+        # We will use the license found on the start page
         if not license_found:
             loader.add_value('license', response.meta['license'])
 
-        return loader.load_item()
+        return {
+            "item": loader.load_item(),
+            "language": language
+            # We need to return the language separately in order to manage the many to many relation
+        }
 
-
+    # Given a couple (key, elem), obtained during the scraping, he returns the valid couple (key1, value1)
+    # to add to the db. If key is not valid, he will return the tuple (None, None)
     @staticmethod
     def get_key_value(key, elem):
         if key == 'Initial release':
@@ -88,11 +105,17 @@ class LibraryOrFrameworkSpider(scrapy.Spider):
         elif key == 'Type':
             return ('type', elem.xpath('./td/a/text()').extract_first())
         elif key == 'License': #There may be a combination of link and plain text
-            value = elem.xpath('./td/a/text() | ./td/text()').extract_first()
-            plain_text = elem.xpath('./td/text()').extract_first()
-            value = value + plain_text if plain_text else value
-            return ('license', value)
+            value = elem.xpath('./td').extract_first()
+            parsed_value = re.sub(r'\[.*?\]', ' ', cleanhtml(value))
+            return ('license', parsed_value)
         elif key == 'Website':
-            return ('website', elem.xpath('./td/span/a/@href').extract_first())
+            urls = [url for url in elem.xpath('./td//a/@href').extract()]
+            return ('website', ','.join(urls))
         else:
             return (None, None)
+
+# Remove all the html tags in the string
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
