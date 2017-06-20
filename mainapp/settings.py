0ebe7833473bff0dec38414170773a57460410a5
darioskal <dipasquale.d@tiscali.it>
2017-06-20 16:25:22 +0200

    errore file statici

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WEBPACK_LOADER = {
     'DEFAULT': {
